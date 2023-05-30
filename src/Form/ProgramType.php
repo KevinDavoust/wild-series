54b9f82104f8058a4fae354f99defe350627dbb7
KevinDavoust <kevin.davoust@orange.fr>
2023-05-30 11:55:40 +0200

    quete 19

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,11 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
@@ -17,6 +19,12 @@ class ProgramType extends AbstractType
             ->add('synopsis', TextType::class)
             ->add('poster')
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+            ]);
         ;
     }
 
